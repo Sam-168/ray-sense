aadf8ea27d71adff2b33a344361eba135cb35613
Sam-168 <samukelondlela2.0@gmail.com>
2026-04-22 16:37:05 +0200

    added ErrorResponse class for global exception handling

---
+++ src/main/java/com/attendance/face/dto/response/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.attendance.face.dto.response;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ErrorResponse {
+    private boolean success;
+    private String message;
+    private LocalDateTime timestamp;
+    private String path;
+
+
+    public ErrorResponse(String message) {
+        this.success = false;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+    
+    public ErrorResponse(String message, String path) {
+        this.success = false;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+        this.path = path;
+    }
+}
