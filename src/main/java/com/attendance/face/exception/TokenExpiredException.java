dbf79e95da0533894255360802ebca7bb87d5fb3
Sam-168 <samukelondlela2.0@gmail.com>
2026-04-28 01:55:38 +0200

    added TokenExpiredException

---
+++ src/main/java/com/attendance/face/exception/TokenExpiredException.java
@@ -0,0 +1,11 @@
+package com.attendance.face.exception;
+
+public class TokenExpiredException extends RuntimeException{
+    public TokenExpiredException(String message) {
+        super(message);
+    }
+
+    public TokenExpiredException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
