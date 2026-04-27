662c6e8d31bce47216069e666ba461215a6ca210
Sam-168 <samukelondlela2.0@gmail.com>
2026-04-28 01:02:07 +0200

    added LoginResponse dto

---
+++ src/main/java/com/attendance/face/dto/response/LoginResponse.java
@@ -0,0 +1,22 @@
+package com.attendance.face.dto.response;
+
+import com.attendance.face.entity.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private UserRole role;
+    private Long userId;
+    private String email;
+    private String fullName;
+
+    
+    private Long studentId;
+    private Long lecturerId;
+    private Long adminId;
+}
