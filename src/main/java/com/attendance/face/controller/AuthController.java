6e6fe3dc48083b2da03653ee27f33466d0513862
Sam-168 <samukelondlela2.0@gmail.com>
2026-04-28 03:55:19 +0200

    fixed AuthController error

--- src/main/java/com/attendance/face/controller/AuthController.java
+++ src/main/java/com/attendance/face/controller/AuthController.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/auth")
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
 
