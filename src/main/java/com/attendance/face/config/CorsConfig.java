4311f71222259c24ee3777ada74e69760a5defeb
Sam-168 <samukelondlela2.0@gmail.com>
2026-05-01 03:36:53 +0200

    fixed the clashing of Spring security and CORS

--- src/main/java/com/attendance/face/config/CorsConfig.java
+++ src/main/java/com/attendance/face/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.attendance.face.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -8,6 +9,7 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 import java.util.Collections;
 
+@Profile("disabled")
 @Configuration
 public class CorsConfig {
     @Bean
