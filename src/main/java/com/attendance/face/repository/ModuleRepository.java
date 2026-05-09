31bbdcbf2c2e8decfd9e7500db0f9bbeba6bd167
Sam-168 <samukelondlela2.0@gmail.com>
2026-05-09 02:35:08 +0200

    added Module Repository

---
+++ src/main/java/com/attendance/face/repository/ModuleRepository.java
@@ -0,0 +1,12 @@
+package com.attendance.face.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ModuleRepository extends JpaRepository<Module, Long> {
+    Optional<Module> findByModuleCode(String moduleCode);
+    boolean existsByModuleCode(String moduleCode);
+}
