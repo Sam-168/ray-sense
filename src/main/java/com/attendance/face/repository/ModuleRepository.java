6775c4e5347949922613a0d90301826ab2098ba3
Sam-168 <samukelondlela2.0@gmail.com>
2026-05-09 03:31:24 +0200

    fixed import on Module repo

--- src/main/java/com/attendance/face/repository/ModuleRepository.java
+++ src/main/java/com/attendance/face/repository/ModuleRepository.java
@@ -2,7 +2,7 @@ package com.attendance.face.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import com.attendance.face.entity.Module;
 import java.util.Optional;
 
 @Repository
