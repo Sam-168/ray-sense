d563faed27c97df906c34bb35085af1df5215e84
Sam-168 <samukelondlela2.0@gmail.com>
2026-05-10 08:01:33 +0200

    updated entities to remove @Data annotations as it was causing a concurrentmodificationexception

--- src/main/java/com/attendance/face/entity/Attendance.java
+++ src/main/java/com/attendance/face/entity/Attendance.java
@@ -1,9 +1,7 @@
 package com.attendance.face.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -11,7 +9,8 @@ import java.time.LocalTime;
 
 @Entity
 @Table(name = "atttendance_records")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Attendance {
