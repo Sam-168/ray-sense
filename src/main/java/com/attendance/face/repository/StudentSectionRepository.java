bc4fdbb3ecaea376400643e135065ff7e954a662
Sam-168 <samukelondlela2.0@gmail.com>
2026-05-10 05:50:48 +0200

    added a findByStudentIdAndSectionId method repo for admin controller

--- src/main/java/com/attendance/face/repository/StudentSectionRepository.java
+++ src/main/java/com/attendance/face/repository/StudentSectionRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StudentSectionRepository extends JpaRepository<StudentSection, Long> {
     List<StudentSection> findByStudent(Student student);
+
+    Optional<StudentSection> findByStudentIdAndSectionId(Long studentId, Long sectionId);
 }
